543e89a56c8f4088e2ff9186fbc638de3111052d
Hollerith47 <hermanhmakiese@gmail.com>
2023-02-19 08:14:30 +0300

    first commit

---
+++ precache-manifest.2a6626632078a07c3ba9b37bfadd7518.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23392f10be43adc80970",
+    "url": "/static/css/main.b7776aa0.chunk.css"
+  },
+  {
+    "revision": "23392f10be43adc80970",
+    "url": "/static/js/main.23392f10.chunk.js"
+  },
+  {
+    "revision": "e31dbceab52948c00c59",
+    "url": "/static/js/1.e31dbcea.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cb544ccfd4d98fb13f0dd3e41d69ff24",
+    "url": "/index.html"
+  }
+];
